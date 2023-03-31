a1c66b7ef12aa2fc65f1ef6947c5f9200d113102
Lfu001 <goodspeed913@gmail.com>
2023-03-31 14:56:51 +0900

    feat: Add public function exports and main code to compute mean, median, and mode

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,7 @@
 pub mod mean;
 pub mod median;
 pub mod mode;
+
+pub use mean::mean;
+pub use median::median;
+pub use mode::mode;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod functions;
+use functions::{mean, median, mode};
+
 fn main() {
-    println!("Hello, world!");
+    // あるクラスのテストの点数が与えられたとする
+    let scores = [80, 90, 75, 85, 90, 92, 88, 80, 78];
+    println!("このクラスの点数は{:?}です。", scores);
+
+    // 平均点を求める
+    let mean = mean(&scores);
+    println!("このクラスの平均点は{}点です。", mean);
+
+    // 中央値を求める
+    let median = median(&scores);
+    println!("このクラスの中央値は{}点です。", median);
+
+    // 最頻値を求める
+    let mode = mode(&scores);
+    match mode {
+        Some(m) => println!("このクラスの最頻値は{}点です。", m),
+        None => println!("このクラスに最頻値はありません。"),
+    }
 }
